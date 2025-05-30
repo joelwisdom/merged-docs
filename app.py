ff63be73e4884728877848964986dd931ff699d4
Joel Wisdom <91797361+joelwisdom@users.noreply.github.com>
2025-05-30 11:54:40 -0500

    update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def merge():
     return send_file(zip_buffer, mimetype='application/zip', download_name='merged_letters.zip', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
